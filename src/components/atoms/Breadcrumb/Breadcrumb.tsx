860ecbfe61f38e67e89270c15a0a381370a7a998
ing.dacastillop <ing.dacastillop@gmail.com>
2025-05-26 09:26:38 -0500

    :art: Improve styles components Card and Breadcrumb

--- src/components/atoms/Breadcrumb/Breadcrumb.tsx
+++ src/components/atoms/Breadcrumb/Breadcrumb.tsx
@@ -1,10 +1,12 @@
 import { useMemo } from 'react';
 import { renderClassStatus } from '../../../helpers';
+import { RlsTheme } from '../../definitions';
 import './Breadcrumb.css';
 
 export interface BreadcrumbLabel {
   label: string;
   onClick?: () => void;
+  rlsTheme?: RlsTheme;
 }
 
 interface BreadcrumbProps {
@@ -23,7 +25,11 @@ function RlsBreadcrumbLabel({ label }: BreadcrumbLabelProps) {
   }, [label.onClick]);
 
   return (
-    <label className="rls-breadcrumb__label" onClick={label.onClick}>
+    <label
+      className="rls-breadcrumb__label"
+      onClick={label.onClick}
+      rls-theme={label.rlsTheme}
+    >
       <a className={className}>{label.label}</a>
     </label>
   );
