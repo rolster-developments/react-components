644ed3c76c9e1e0d7cb9261b4a9a97df51a58e70
ing.dacastillop <ing.dacastillop@gmail.com>
2026-01-21 12:24:38 -0500

    :art: Improve style by components Badge and Avatar

--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -4,19 +4,21 @@ import { RlsComponent } from '../../definitions';
 import './Avatar.css';
 
 interface AvatarProps extends RlsComponent {
+  contrast?: boolean;
   rounded?: boolean;
   skeleton?: boolean;
 }
 
 export function RlsAvatar({
   children,
+  contrast,
   rounded,
   skeleton,
   rlsTheme
 }: AvatarProps) {
   const className = useMemo(() => {
-    return renderClassStatus('rls-avatar', { rounded, skeleton });
-  }, [rounded, skeleton]);
+    return renderClassStatus('rls-avatar', { contrast, rounded, skeleton });
+  }, [contrast, rounded, skeleton]);
 
   return (
     <div className={className} rls-theme={rlsTheme}>
